24f45b4683a5ed727da297e8ea666a8307da6118
Mario Giampietri <mario.giampietri@gmail.com>
2022-02-02 21:41:23 +0000

    Set auto visited mode on by default

--- init.el
+++ init.el
@@ -143,6 +143,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-default t)
+ '(auto-save-visited-mode t)
  '(coffee-tab-width 2)
  '(package-selected-packages
    '(clj-refactor no-littering auto-package-update haskell-mode terraform-mode magit tagedit rainbow-delimiters projectile smex ido-completing-read+ cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell)))
