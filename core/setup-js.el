9c7ffa38e7909ea1e6dde324b2ddd4a48cf26978
Mario Giampietri <mario.giampietri@gmail.com>
2022-04-19 22:49:37 +0100

    Better defaults for JS + removed HTML settings (not used ATM)

--- core/setup-js.el
+++ core/setup-js.el
@@ -1,12 +1,15 @@
-;; javascript / html
-(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
-(add-hook 'js-mode-hook 'subword-mode)
-(add-hook 'html-mode-hook 'subword-mode)
-(setq js-indent-level 2)
-
-(use-package tagedit
+(use-package js2-mode
+  :mode ("\\.js\\'" . js2-mode)
+  :interpreter ("node" . js2-mode)
+  :init (setq js-indent-level 2)
   :config
-  (eval-after-load "sgml-mode"
-    '(progn
-       (tagedit-add-paredit-like-keybindings)
-       (add-hook 'html-mode-hook (lambda () (tagedit-mode 1))))))
+  (use-package js2-refactor
+    :diminish
+    :hook (js2-mode . js2-refactor-mode)
+    :config (js2r-add-keybindings-with-prefix "C-c C-m"))
+  (use-package xref-js2
+    :config
+    (define-key js2-mode-map (kbd "M-.") nil)
+    (add-hook 'js2-mode-hook (lambda ()
+                               (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+    (setq xref-js2-search-program 'rg)))
