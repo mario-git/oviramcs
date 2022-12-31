a6b531aaf7b814e0534672490571c15784694cf4
Mario Giampietri <mario.giampietri@gmail.com>
2022-12-31 01:27:31 +0000

    vanilla: removed redundant :ensure statement

--- vanilla/init.el
+++ vanilla/init.el
@@ -65,7 +65,6 @@
 
 ;; https://github.com/emacs-dashboard/emacs-dashboard
 (use-package dashboard
-  :ensure t
   :config
   (dashboard-setup-startup-hook)
   (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*"))
