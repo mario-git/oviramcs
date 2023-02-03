389f8502aa90c8ce9a2318395ad1d4c89bad6430
Mario Giampietri <mario.giampietri@gmail.com>
2023-02-03 23:36:22 +0000

    vanilla: restored treemacs default width

--- vanilla/init.el
+++ vanilla/init.el
@@ -264,8 +264,7 @@
   :config
   (setq treemacs-project-follow-cleanup t
 	treemacs-show-hidden-files nil
-	treemacs-text-scale 0.2
-	treemacs-width 30) ;; TODO: tune this based on OS type
+	treemacs-text-scale 0.2)
   (treemacs-resize-icons 16)
   ;; name function in case we need to add the same hook somewhere else
   (add-hook 'treemacs-mode-hook (lambda() (display-line-numbers-mode -1)))
